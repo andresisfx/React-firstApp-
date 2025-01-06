6a266f44f15639b70324814df500f822050f95f4
andresisfx <andresrodriguezwg@gmail.com>
2025-01-05 22:22:01 -0500

    using getAllByText and getByTestId

--- test/FirstApp.test.jsx
+++ test/FirstApp.test.jsx
@@ -6,7 +6,7 @@ describe('Pruebas en el componente <FirstApp />', () => {
     test('Debe hacer match con el snapshot', () => {
       
       const tittle = 'Hola, Soy Vegeta';
-      const { container,  } = render( <FirstApp title={ tittle }/> );
+      const { container,  } = render( <FirstApp title={tittle}/> );
 
       expect( container ).toMatchSnapshot();
     })
@@ -14,12 +14,24 @@ describe('Pruebas en el componente <FirstApp />', () => {
 
     test('Debe mostrar el título en  un h1 ', () => {
         
-        const tittle = 'Hola, Soy Vegeta';
-        const { container, getByText  } = render( <FirstApp title={ tittle } /> );
+        const title = 'Hola, Soy Vegeta';
+        const { container, getByText, getByTestId  } = render( <FirstApp title={title} /> );
   
-        expect( getByText(tittle) ).toBeTruthy();
-        const h1 = container.querySelector('h1').innerHTML;
-        expect( h1 ).toContain(tittle);
-        
+        // expect( getByText(title) ).toBeTruthy();
+        // const h1 = container.querySelector('h1').innerHTML;
+        // expect( h1 ).toContain(tittle);
+
+        expect( getByTestId('test-title').innerHTML ).toContain(title);
+
       })
+
+    test('Debe mostrar el subtitulo enviado por props', () => {
+
+        const tittle = 'Hola, Soy Vegeta';
+        const subTitle = 'Soy un subtitulo';
+        const { container, getAllByText } = render( <FirstApp title={ tittle } subTitle={ subTitle } /> );
+
+        expect( getAllByText(subTitle).length ).toBe(2);
+
+    })  
 })
